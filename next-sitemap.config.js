094936240f6e9b7887d588051af3e49dc9307b4f
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2025-05-02 13:42:45 +0100

    chore: Use correct url for sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ovansa.com',
+  siteUrl: 'https://ovansa.me',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
